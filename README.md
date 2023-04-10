b4fded68bf74fe08224a692d25f965551bf54de6
Carlos Barros <56875807+carlosefrbarros@users.noreply.github.com>
2023-04-10 13:41:26 -0300

    Completed Project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Web Scraping Automotive News
- 
+
+This project was made to help a friend in his academic research, he is researching news about eletric cars in Brazil
+With the URLs from his Zotero references, I scraped the content and saved in .txt files
--- web_scraping_script.py
+++ web_scraping_script.py
@@ -1,11 +1,32 @@
-# Web Scraping Automobile News
+# Web Scraping Automotive News
+# This project was made to help a friend in his academic research, he is researching news about eletric cars in Brazil
+# With the URLs from his Zotero references, I scraped and saved in .txt files
 
-# read csv to dataframe
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+url_excel = 'D:/INFO/Projetos Ciência de Dados/Web-Scraping-Automobile-News/References.xlsx'
+output_folder = 'D:/INFO/Projetos Ciência de Dados/Web-Scraping-Automobile-News/Text'
+
+# read xlsx to dataframe
+df = pd.read_excel(url_excel)
 
 # loop through url column of dataframe
+for i, row in df.iterrows():
 
-# request to the urls
+    # request to the urls
+    response = requests.get(row['Url'])
 
-# scrape urls
+    # scrape urls
+    soup = BeautifulSoup(response.content, 'html.parser')
+    url_title = soup.find('title')
+    url_content = soup.find('div', class_='post-area__text')
+    url_text = url_title.text.strip().replace(' | Automotive Business', '') + '\n' + url_content.text.strip()
+    print('Scraped URL {}'.format(i+1))
 
-# write content to .txt file
+    # write content to .txt file
+    adress = output_folder + '/URL_' + str(i+1) + '.txt'
+    text_file = open(adress, 'w', encoding='utf-8')
+    text_file.write(url_text)
+    text_file.close()
